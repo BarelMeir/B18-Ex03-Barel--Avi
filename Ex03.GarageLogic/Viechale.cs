44bbbbd4ee8651f1d2bf0922aee2fadc7fde739c
BarelMeir <barel.meir@post.idc.ac.il>
2018-05-10 17:13:01 +0300

    viechale basic
    getters and setters.
    
    need to add exeption at Wheel -> Inflate

--- Ex03.GarageLogic/Viechale.cs
+++ Ex03.GarageLogic/Viechale.cs
@@ -1,13 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Text;
 
 namespace Ex03.GarageLogic
 {
-    class Viechale
-    {
-        private int x = 1;
 
+    public enum eRepairStatus
+    {
+        InProgress,
+        Completed,
+        Paid
+    }
+
+    public class Viechale
+    {
+        private class Wheel
+        {
+            private string m_ManufactorName;
+            private float m_CurrentAirPressure;
+            private float m_MaxAirPressure;
+
+            public string ManufactorName
+            {
+                get { return m_ManufactorName; }
+                set { m_ManufactorName = value; }
+            }
+
+            public float CurrentAirPressure
+            {
+                get { return m_CurrentAirPressure; }
+                set { m_CurrentAirPressure = value; }
+            }
+
+            public float MaxAirPressure
+            {
+                get { return m_MaxAirPressure; }
+                set { m_MaxAirPressure = value; }
+            }
+
+            internal void Inflate(float i_MountToInflate)
+            {
+                if (m_CurrentAirPressure + i_MountToInflate <= m_MaxAirPressure)
+                {
+                    m_CurrentAirPressure += i_MountToInflate;
+                }
+                else
+                {
+                    // TODO exeption
+                }
+            }
+        }
+
+        private string m_ModelName;
+        private string m_LicenseNumber;
+        private float m_EnergyLeftPrecentage;
+        private LinkedList<Wheel> m_Wheels;
+        private eRepairStatus m_RepairStatus;
+
+        public string ModelName
+        {
+            get { return m_ModelName; }
+            set { m_ModelName = value; }
+        }
+
+        public string LicenseNumber
+        {
+            get { return m_LicenseNumber; }
+            set { m_LicenseNumber = value; }
+        }
+
+        public float EnergyLeftPrecentage
+        {
+            get { return m_EnergyLeftPrecentage; }
+            set { m_EnergyLeftPrecentage = value; }
+        }
+
+        public eRepairStatus RepairStatus
+        {
+            get { return m_RepairStatus; }
+            set { m_RepairStatus = value; }
+        }
+
+        public void InflateAllWheels(float io_MountToInflate)
+        {
+            foreach (Wheel currentWheel in m_Wheels)
+            {
+                currentWheel.Inflate(io_MountToInflate);
+            }
+        }
 
     }
 }
